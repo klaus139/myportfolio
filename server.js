ee8a23f28607ccee25cf582e525b2f3face76c5e
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2023-01-05 08:50:22 +0100

    changed the link

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.use(cors());
 
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static("client/build"))
+    app.use(express.static("client/public"))
     app.get("*", (req, res) => 
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+        res.sendFile(path.resolve(__dirname, "client", "public", "index.html"))
     );
 };
 app.use("/api", contactRoute)
